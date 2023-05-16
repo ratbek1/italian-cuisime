1365950b812b79481dcb88a96d0f7a33f74b7ad6
chegebaevraatbek <tasabekovraatbek999@gmail.com>
2023-05-16 18:34:55 +0600

    hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div>
-            
+        <div id="hero">
+            <div className="container text-white text-center py-60">
+                <div className="flex items-end justify-center">
+                    <div className="w-[100px] h-[5px] bg-red-600"></div>
+                    <h2 className="">Delicious</h2>
+                    <div className="w-[100px] h-[5px] bg-red-600"></div>
+                </div>
+                <h1 className="font-bold text-6xl">Italian Cuisine</h1>
+                <p className="w-[32%] mx-auto">Classic steak & delicious with delightfully unexpenced twists.
+                    The Restaurant`s sunny decor was inspired by the diners</p>
+                <div>
+                    <button>Reserve Your Table </button>
+                </div>
+            </div>
         </div>
     );
 };
