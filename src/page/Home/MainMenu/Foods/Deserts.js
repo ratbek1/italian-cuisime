5410c0ae7c23106b936d68851b4d093cb4d3eb75
ratbek1 <115623152+ratbek1@users.noreply.github.com>
2023-05-25 21:18:03 +0600

    Finishing (#7)
    * end
    
    * new
    
    * slick
    
    ---------
    
    Co-authored-by: nurzigit-mg <turdumamatovnurzhigit@gmail.com>
    Co-authored-by: chegebaevraatbek <tasabekovraatbek999@gmail.com>

---
+++ src/page/Home/MainMenu/Foods/Deserts.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Desserts} from "../../../../FakeBack/DataMenu";
+
+const Deserts = () => {
+    return (
+        <div>{
+            Desserts.map((el) => (
+                <div className="">
+                    <div className="flex items-end pb-3">
+                        <h1 className="text-[#FFFFFF] font-serif text-base line-[31px]  font-Silk">
+                            {el.name}
+                        </h1>
+                        <h5 className="px-5 text-[#FFFFFF] font-serif text-xs line-[31px] font-Silk opacity-50">
+                            .....................................................................................
+                        </h5>
+                        <h2 className="text-[#EF272C] font-semibold text-base line-[31px] font-Silk"> $ {el.price}</h2>
+                    </div>
+                    <div className="flex justify-start pb-6">
+                        <p className="text-[#FFFFFF] font-normal text-xs w-96  line-[20px] flex-wrap font-sans">{el.text}</p>
+                        <a href="" className="text-[#FFFFFF] text-base pl-7 pt-3 underline font-Silk">Order Now</a>
+                    </div>
+                </div>
+
+            ))
+        }
+        </div>
+    );
+};
+
+export default Deserts;
---
+++ src/page/Home/MainMenu/Foods/HotDrinks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotDrinks = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HotDrinks;
